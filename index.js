8137f0ef104e79485ee1f02c513ed7ff19874281
Zahari Stoianov <zahari3301@gmail.com>
2020-08-31 19:48:12 +0300

    bug fix & some logic improvements

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import "./src/Navigation";
 
-const nav = new Navigation({
-    selector: '#navigation',
-    itemSelector: '.anchor',
-    withControl: true,
-    withDraggable: true,
-    anchorAnimationEase: 'ease',
-    anchorAnimationDuration: '0.3s',
-    leftArrowSelector: '.buttons .left',
-    rightArrowSelector: '.buttons .right',
-    onAnchorChange: (anchor) => {
-        console.log('Anchor Changed')
-    },
-    onMount: () => { 
-        console.log('Navigation Mounted')
-    },
-    onSectionReached: () => { 
-        console.log('Section Reached')
-    },
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const nav = new Navigation({
+        selector: '#navigation',
+        anchorSelector: '.anchor',
+        withControl: true,
+        withDraggable: true,
+        anchorAnimationEase: 'ease',
+        anchorAnimationDuration: '0.3s',
+        leftArrowSelector: '.buttons .left',
+        rightArrowSelector: '.buttons .right',
+        onAnchorChange: (anchor) => {
+            console.log('Anchor Changed')
+        },
+        onMount: () => {
+            console.log('Navigation Mounted')
+        },
+        onSectionReached: () => {
+            console.log('Section Reached')
+        },
+    });
+    nav.mount();
 });
 
-nav.mount();
